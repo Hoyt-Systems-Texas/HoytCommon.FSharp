5c413c7f8095425182a3d39c735923b749972579
Matt Hoyt <mrhoyt4@gmail.com>
2020-09-14 05:05:12 -0500

    Added a message type that uses ADTs.

--- HoytTech.Messaging/Message.fs
+++ HoytTech.Messaging/Message.fs
@@ -1,2 +1,40 @@
 module HoytTech.Messaging.Message
 
+open System
+open HoytTech.Core
+
+module Request =
+    
+    [<CLIMutable>]
+    type t<'request> = {
+        requestId: int64 ref
+        requestedBy: Guid
+        request: 'request
+        userId: option<Guid>   
+    }
+
+module Reply =
+    
+    [<CLIMutable>]
+    type t<'reply> = {
+        requestId: int64
+        responseTo: Guid
+        result: ResultMonad.t<'reply>
+    }
+    
+module Event =
+    
+    [<CLIMutable>]
+    type t<'msg> = {
+        sendTo: Guid
+        msg: 'msg
+    }
+    
+type t<'request, 'reply, 'event> =
+    | Request of Request.t<'request>
+    | Reply of Reply.t<'reply>
+    | Event of Event.t<'event>
+    | Ping
+    | Pong
+
+
